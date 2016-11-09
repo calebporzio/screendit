7f64280170450dcfa9864fac4a2eafbfeb99a883
Caleb Porzio <caleb@fmtc.co>
2016-11-08 23:27:19 -0500

    set a timeout and some profiling code for optimization

--- rasterize.js
+++ rasterize.js
@@ -1,3 +1,6 @@
+// Timer
+var start = new Date().getTime();
+
 "use strict";
 var page = require('webpage').create(),
     system = require('system');
@@ -18,12 +21,26 @@ if (system.args.length !== 8) {
     // Set viewport size
     page.viewportSize = { width: viewportWidth, height: viewportHeight };
 
+    // Set timeout on page load.
+    page.settings.resourceTimeout = 5000; // in milliseconds
+
     // sets the crop for the output image
     if (outputWidth != 0 && outputHeight != 0) {
         page.clipRect = { top: 0, left: 0, width: outputWidth, height: outputHeight };
     }
 
+    // Timer
+    var end = new Date().getTime();
+    console.log('Boot: ' + String((end - start) / 1000))
+    var startOpen = new Date().getTime();
+
     page.open(sourceUrl, function (status) {
+
+        // Timer
+        var endOpen = new Date().getTime();
+        console.log('Page Open: ' + String((endOpen - startOpen) / 1000))
+        var start = new Date().getTime();
+
         if (status !== 'success') {
             console.error('Error: Unable to load the address');
             phantom.exit(1);
@@ -33,6 +50,11 @@ if (system.args.length !== 8) {
                     page.injectJs('hide_lightboxes.js');
                 }
                 page.render(outputFile);
+
+                // Timer
+                var end = new Date().getTime();
+                console.log('Page Render: ' + String((end - start) / 1000))
+
                 phantom.exit();
             }, 100);
         }
--- screenshot.js
+++
@@ -1,8 +0,0 @@
-var page = require('webpage').create();
-page.open('https://tesla.com', function(status) {
-  console.log("Status: " + status);
-  if(status === "success") {
-    page.render('google.png');
-  }
-  phantom.exit();
-});
